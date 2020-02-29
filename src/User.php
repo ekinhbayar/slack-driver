729de049f24a0e887457c609428dfc2f615f080f
ekinhbayar <me@ekins.space>
2020-02-29 19:58:39 +0300

    Add a User class Prior to this change, the user information was represented as a string.
    This change introduces a concrete User class to represent
    the user information we receive from the Slack API.

---
+++ src/User.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace ekinhbayar\Driver\Slack;
+
+class User
+{
+    private string  $identifier;
+
+    private ?string $name;
+
+    public function __construct(string $identifier, ?string $name)
+    {
+        $this->identifier = $identifier;
+        $this->name       = $name;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
