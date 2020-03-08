01f10cfe51a9f24ed5d818131c34d9da41259edd
ekinhbayar <me@ekins.space>
2020-03-08 21:48:31 +0300

    Parse user and channel separately when building channel message This commit also introduces an early return for generic channel messages

--- src/Webhook/RequestParser/NewChannelMessage.php
+++ src/Webhook/RequestParser/NewChannelMessage.php
@@ -2,14 +2,19 @@
 
 namespace ekinhbayar\Driver\Slack\Webhook\RequestParser;
 
-use ekinhbayar\Driver\Slack\Channel;
 use ekinhbayar\Driver\Slack\Event\Data\ChannelMessage;
+use ekinhbayar\Driver\Slack\Event\Data\Mention;
+use ekinhbayar\Driver\Slack\Event\Data\ValueObject\Channel;
+use ekinhbayar\Driver\Slack\Event\Data\ValueObject\User;
 
-class NewChannelMessage
+final class NewChannelMessage
 {
     /** @var array<string,mixed> */
     private array $eventData;
 
+    /**
+     * @param array<string,mixed> $eventData
+     */
     public function __construct(array $eventData)
     {
         $this->eventData = $eventData;
@@ -17,12 +22,31 @@ class NewChannelMessage
 
     public function parse(): ChannelMessage
     {
-        return new ChannelMessage(
+        $eventData = [
             $this->eventData['token'],
             $this->eventData['event']['text'],
-            $this->eventData['event']['user'],
+            $this->parseUser(),
             \DateTimeImmutable::createFromFormat('U.u', $this->eventData['event']['ts']),
-            new Channel($this->eventData['event']['channel'], $this->eventData['event']['channel_type'])
+            $this->parseChannel(),
+        ];
+
+        if ($this->eventData['event']['type'] === 'message') {
+            return new ChannelMessage(...$eventData);
+        }
+
+        return new Mention(...$eventData);
+    }
+
+    private function parseUser(): User
+    {
+        return new User($this->eventData['event']['user']);
+    }
+
+    private function parseChannel(): Channel
+    {
+        return new Channel(
+            $this->eventData['event']['channel'],
+            $this->eventData['event']['channel_type'],
         );
     }
 }
