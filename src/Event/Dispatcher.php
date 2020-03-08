655f58989fc2ecdacc2464cf4117e083bd7d68a2
ekinhbayar <me@ekins.space>
2020-03-08 21:18:17 +0300

    Move user and channel to correct namespace
    Move event dispatcher under Event
    
    OnNewMention -> OnMention

--- src/Event/Dispatcher.php
+++ src/Event/Dispatcher.php
@@ -8,9 +8,11 @@ use Amp\Http\Status;
 use Amp\Promise;
 use AsyncBot\Core\Http\WebHookListener;
 use ekinhbayar\Driver\Slack\Event\Data\ChannelMessage;
+use ekinhbayar\Driver\Slack\Event\Data\Command;
 use ekinhbayar\Driver\Slack\Event\Data\Factory;
 use ekinhbayar\Driver\Slack\Event\Data\Mention;
 use ekinhbayar\Driver\Slack\Event\Listener\OnNewChannelMessage;
+use ekinhbayar\Driver\Slack\Event\Listener\OnNewCommand;
 use ekinhbayar\Driver\Slack\Event\Listener\OnNewMention;
 use function Amp\call;
 
@@ -18,7 +20,7 @@ class EventDispatcher implements WebHookListener
 {
     private array $listeners = [
         ChannelMessage::class => [],
-        Mention::class => [],
+        Command::class => [],
     ];
 
     public function addChannelMessageEventListener(OnNewChannelMessage $channelMessageListener): void
@@ -28,12 +30,14 @@ class EventDispatcher implements WebHookListener
 
     public function addMentionEventListener(OnNewMention $mentionListener): void
     {
-        $this->listeners[Mention::class][] = $mentionListener;
+        $this->listeners[ChannelMessage::class][] = $mentionListener;
     }
 
-    /**
-     * @inheritDoc
-     */
+    public function addCommandEventListener(OnNewCommand $mentionListener): void
+    {
+        $this->listeners[Command::class][] = $mentionListener;
+    }
+
     public function __invoke(Request $request): Promise
     {
         return call(function () use ($request) {
